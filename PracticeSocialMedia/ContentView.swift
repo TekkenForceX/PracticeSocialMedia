2c6bbb3d4266ff8b1629ad85fdf6752eb05d7569
Matthew Fails <matthew.c.fails.com>
2024-05-24 12:53:54 -0400

    Initial Commit

---
+++ PracticeSocialMedia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeSocialMedia
+//
+//  Created by Matthew Fails on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeSocialMedia/PracticeSocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSocialMediaApp.swift
+//  PracticeSocialMedia
+//
+//  Created by Matthew Fails on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
